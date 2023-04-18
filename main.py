98f433d52161f094bf40a759ab6f9f5806575699
ChawinSriviset <64011091@kmitl.ac.th>
2023-04-19 00:07:26 +0700

    .

--- main.py
+++ main.py
@@ -134,11 +134,59 @@ games = [
     Game("Phasmophobia", 350, "A horror game where you and your team investigate haunted locations and try to capture evidence of ghosts"),
 ]
 
+# Wishlist class
+class Wishlist:
+    wishlists = {}
+
+    def __init__(self, user_id):
+        self._user_id = user_id
+        self._wishlist = []
+        Wishlist.wishlists[self._user_id] = self
+
+    def add_game(self, game):
+        self._wishlist.append(game)
+        return {"message": f"Added {game.name} to wishlist."}
+
+    def remove_game(self, game):
+        if game in self._wishlist:
+            self._wishlist.remove(game)
+            return {"message": f"Removed {game.name} from wishlist."}
+        else:
+            return {"error": "GameNotFound"}
+
+    def view_wishlist(self):
+        if len(self._wishlist) == 0:
+            return {"message": "Your wishlist is empty."}
+        else:
+            games = [{"name": game.name, "price": game.price} for game in self._wishlist]
+            return {"games": games}
+
 # FastAPI route handlers
 @app.get("/games")
 async def get_games():
     game_dicts = [game.to_dict() for game in games]
     return game_dicts
+
+# FastAPI route handlers for wishlist
+@app.post("/users/{user_id}/wishlist/add_game")
+async def add_game_to_wishlist(user_id: int, game_index: int):
+    user = User.users[user_id]
+    wishlist = Wishlist(user_id)
+    game = games[game_index]
+    return wishlist.add_game(game)
+
+@app.post("/users/{user_id}/wishlist/remove_game")
+async def remove_game_from_wishlist(user_id: int, game_index: int):
+    user = User.users[user_id]
+    wishlist = Wishlist(user_id)
+    game = games[game_index]
+    return wishlist.remove_game(game)
+
+@app.get("/users/{user_id}/wishlist/view_wishlist")
+async def view_wishlist(user_id: int):
+    user = User.users[user_id]
+    wishlist = Wishlist(user_id)
+    return wishlist.view_wishlist()
 
 @app.post("/users/{user_id}/cart/add_game")
 async def add_game_to_cart(user_id: int, game_index: int):
@@ -165,3 +213,19 @@ async def get_total_price(user_id: int):
     user = User.users[user_id]
     cart = Cart(user_id)
     return {"total_price": cart.total_price()}
+
+# FastAPI route handlers for payment
+@app.post("/users/{user_id}/payment/pay_with_credit_card")
+async def make_payment_with_credit_card(user_id: int, card_name: str, card_number: str, expiration: str, cvv: str, save_status: str):
+    purchase = Purchase(user_id)
+    return purchase.pay_with_credit_card(card_name, card_number, expiration, cvv, save_status)
+
+@app.post("/users/{user_id}/payment/pay_with_paypal")
+async def make_payment_with_paypal(user_id: int, email: str, password: str, save_status: str):
+    purchase = Purchase(user_id)
+    return purchase.pay_with_paypal(email, password, save_status)
+
+@app.post("/users/{user_id}/payment/use_own_card")
+async def make_payment_with_own_card(user_id: int, payment_type: str, card_name: str):
+    purchase = Purchase(user_id)
+    return purchase.own_card(payment_type, card_name)
