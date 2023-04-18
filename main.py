cef851c6417587e811f9f4e8548bbea2a415da6d
ChawinSriviset <64011091@kmitl.ac.th>
2023-04-18 23:57:54 +0700

    gathered all the codes into one file

--- main.py
+++ main.py
@@ -1,89 +1,167 @@
-import json
 from fastapi import FastAPI
+from pydantic import BaseModel
 import uvicorn
 
 app = FastAPI()
 
+# Game class
 class Game:
-    def __init__(self, name, price, detail):
+    def __init__(self, name, price, details):
         self.name = name
         self.price = price
-        self.detail = detail
+        self.details = details
 
-class GameList:
-    def __init__(self):
-        self.list = []
+    def to_dict(self):
+        return {
+            "name": self.name,
+            "price": self.price,
+            "details": self.details,
+        }
 
-    def add_to_list(self, game):
-        self.list.append(game)
+# User class
+class User:
+    users = {}
 
-    def __str__(self):
-        return '\n'.join(game.name for game in self.list)
-    
-    def __getitem__(self, index):
-        return self.list[index]
+    def __init__(self, username, password, email, user_id):
+        self._username = username
+        self._password = password
+        self._email = email
+        self._user_id = user_id
+        self._purchase_history = {}
+        self._credit_card = {}
+        self._paypal = {}
+        User.users[self._user_id] = self
 
+    def add_game_hs(self, game, price):
+        self._purchase_history[game] = price
+
+    def add_credit_card(self, card_name, card_number, expiration, cvv):
+        self._credit_card[card_name] = CreditCard(card_number, expiration, cvv)
+
+    def add_paypal(self, email, password):
+        self._paypal[email] = Paypal(email, password)
+
+# CreditCard class
+class CreditCard:
+    def __init__(self, card_number, expiration, cvv):
+        self._card_number = card_number
+        self._expiration = expiration
+        self._cvv = cvv
+
+# PayPal class
+class Paypal:
+    def __init__(self, email, password):
+        self._email = email
+        self._password = password
+
+# Cart class
 class Cart:
-    def __init__(self):
-        self.cart = []
+    def __init__(self, user_id):
+        self._user_id = user_id
+        self._cart = []
 
     def add_game(self, game):
-        self.cart.append(game)
+        self._cart.append(game)
         return {"message": f"Added {game.name} to cart."}
 
     def remove_game(self, game):
-        if game in self.cart:
-            self.cart.remove(game)
+        if game in self._cart:
+            self._cart.remove(game)
             return {"message": f"Removed {game.name} from cart."}
         else:
             return {"error": "GameNotFound"}
 
     def view_cart(self):
-        if len(self.cart) == 0:
+        if len(self._cart) == 0:
             return {"message": "Your cart is empty."}
         else:
-            games = [{"name": game.name, "price": game.price} for game in self.cart]
+            games = [{"name": game.name, "price": game.price} for game in self._cart]
             return {"games": games}
 
     def total_price(self):
-        return sum(game.price for game in self.cart)
+        return sum(game.price for game in self._cart)
 
-list1 = GameList()
-cart1 = Cart()
+# Purchase class
+class Purchase:
+    user_purchase = {}
 
+    def __init__(self, user_id):
+        self._user_id = user_id
+        Purchase.user_purchase[self._user_id] = self
+
+    def pay_with_credit_card(self, card_name, card_number, expiration, cvv, save_status):
+        user = User.users[self._user_id]
+        cart = Cart.carts[self._user_id]
+        if save_status.lower() == 'yes':
+            user.add_credit_card(card_name, card_number, expiration, cvv)
+        # Process payment with PayPal
+        for gm in cart.games():
+            user.add_game_hs(gm._name, gm._price)
+        return {f"Paid {cart.total_price()} using Creditcard {card_number}"}
+
+    def pay_with_paypal(self, email, password, save_status):
+        user = User.users[self._user_id]
+        cart = Cart.carts[self._user_id]
+        if save_status.lower() == 'yes':
+            user.add_paypal(email, password)
+        # Process payment with PayPal
+        for gm in cart.games():
+            user.add_game_hs(gm._name, gm._price)
+        return {f"Paid {cart.total_price()} using PayPal account {email}"}
+
+    def own_card(self, payment_type, card_name):
+        user = User.users[self._user_id]
+        if payment_type.lower() == 'creditcard' and card_name in user._credit_card:
+            pm = user._credit_card[card_name]
+            return self.pay_with_credit_card(card_name, pm._card_number, pm._expiration, pm._cvv, 'no')
+        elif payment_type.lower() == 'paypal' and card_name in user._paypal:
+            pm = user._paypal[card_name]
+            return self.pay_with_paypal(pm._email, pm._password, 'no')
+        else:
+            return {"Payment not found"}
+
+# Game list
+games = [
+    Game("GTA V", 1850, "Open-world action-adventure game."),
+    Game("Minecraft", 790, "Sandbox game where you can build and explore."),
+    Game("Fortnite", 0, "A FREE Battle royale game with cartoon graphics."),
+    Game("Hogwart", 1500, "Single-player Action-adventure game"),
+    Game("Skyrim", 1400, "Single-player Action game"),
+    Game("Destiny", 900, "Multi-player FPS game"),
+    Game("Fall Guys", 500, "A battle royale game"),
+    Game("Among Us", 120, "A multiplayer game where you work together to find the imposter on a spaceship"),
+    Game("Valheim", 280, "A Viking-themed survival game where you explore and build in a procedurally generated world"),
+    Game("Phasmophobia", 350, "A horror game where you and your team investigate haunted locations and try to capture evidence of ghosts"),
+]
+
+# FastAPI route handlers
 @app.get("/games")
 async def get_games():
-    return {"games": [{"name": game.name, "price": game.price, "detail": game.detail} for game in list1.list]}
+    game_dicts = [game.to_dict() for game in games]
+    return game_dicts
 
-@app.post("/cart/add_game")
-async def add_game_to_cart(game_index: int):
-    game = list1[game_index]
-    return cart1.add_game(game)
+@app.post("/users/{user_id}/cart/add_game")
+async def add_game_to_cart(user_id: int, game_index: int):
+    user = User.users[user_id]
+    cart = Cart(user_id)
+    game = games[game_index]
+    return cart.add_game(game)
 
-@app.post("/cart/remove_game")
-async def remove_game_from_cart(game_index: int):
-    game = list1[game_index]
-    return cart1.remove_game(game)
+@app.post("/users/{user_id}/cart/remove_game")
+async def remove_game_from_cart(user_id: int, game_index: int):
+    user = User.users[user_id]
+    cart = Cart(user_id)
+    game = games[game_index]
+    return cart.remove_game(game)
 
-@app.get("/cart/view_cart")
-async def view_cart():
-    return cart1.view_cart()
+@app.get("/users/{user_id}/cart/view_cart")
+async def view_cart(user_id: int):
+    user = User.users[user_id]
+    cart = Cart(user_id)
+    return cart.view_cart()
 
-@app.get("/cart/total_price")
-async def get_total_price():
-    return {"total_price": cart1.total_price()}
-
-# Function to handle adding a game to the cart
-@app.post("/cart/add_to_cart")
-async def add_to_cart(game_index: int):
-    game = list1[game_index]
-    cart1.add_game(game)
-    return {"message": f"Added {game.name} to cart."}
-
-# Function to handle displaying the contents of the cart
-@app.get("/cart/view_cart")
-async def view_cart():
-    return cart1.view_cart()
-
-# if __name__ == '__main__':
-#     uvicorn.run(app, host='127.0.0.1', port=8000)
+@app.get("/users/{user_id}/cart/total_price")
+async def get_total_price(user_id: int):
+    user = User.users[user_id]
+    cart = Cart(user_id)
+    return {"total_price": cart.total_price()}
